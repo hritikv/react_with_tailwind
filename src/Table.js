9107d49030dfecbfe143f2d8d6dc1a93b195e320
hritikv <hritik5269@gmail.com>
2023-03-20 11:26:59 +0530

    add all files

---
+++ src/Table.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const Table = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="Table">Select an option:</label>
+      <select id="Table" value={selectedOption} onChange={handleChange}>
+        <option value="">Select an option</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+    </div>
+  );
+};
+
+export default Table;
